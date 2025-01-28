d37a983b081b30aceec504dbeaf17f3d4af13c59
ALVIANRML <alvianrmli@gmail.com>
2025-01-28 21:44:49 +0700

    finish

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const uploadRouter = require('./routes/upload-img');
-const getRouter = require('./routes/get-img')
-const postEvent = require('./routes/upload-events')
-const getEvent = require('./routes/get-event')
-
+const getRouter = require('./routes/get-img');
+const postEvent = require('./routes/upload-events');
+const getEvent = require('./routes/get-event');
 
 const app = express();
 
+// CORS Configuration
+const corsOptions = {
+  origin: 'https://my-frontend.vercel.app', // Ganti dengan domain frontend Anda
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Izinkan metode-metode tertentu
+  allowedHeaders: ['Content-Type', 'Authorization'], // Header yang diizinkan
+};
+
 // Middleware
-app.use(cors()); // Untuk mengizinkan request dari domain lain (CORS)
+app.use(cors(corsOptions)); // Apply CORS globally with custom options
 app.use(express.json({ limit: '10mb' })); // Untuk mem-parsing JSON dengan limit ukuran
 app.use(express.urlencoded({ limit: '10mb', extended: true })); // Untuk mem-parsing data URL-encoded dengan limit ukuran
 app.use(bodyParser.json()); // Untuk mem-parsing JSON
@@ -19,18 +25,16 @@ app.use(bodyParser.urlencoded({ extended: true })); // Untuk mem-parsing data UR
 
 // Routes
 // POST
-app.use("/api", authRoutes); 
-app.use('/api', uploadRouter); 
-app.use('/api', postEvent); 
-
+app.use("/api", authRoutes);
+app.use('/api', uploadRouter);
+app.use('/api', postEvent);
 
 // GET
-app.use("/api", getRouter); 
-app.use("/api", getEvent); 
-
+app.use("/api", getRouter);
+app.use("/api", getEvent);
 
 // Menjalankan server
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
