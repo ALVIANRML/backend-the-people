06d85f6c30d446b43efdd0b4054bf9f98e2f4e6c
ALVIANRML <alvianrmli@gmail.com>
2025-01-29 00:14:42 +0700

    finish

--- server.js
+++ server.js
@@ -8,16 +8,13 @@ const postEvent = require('./routes/upload-events');
 const getEvent = require('./routes/get-event');
 
 const app = express();
-
-// CORS Configuration
 const corsOptions = {
-  origin: 'https://the-people-git-master-yannns-projects.vercel.app', // Ganti dengan domain frontend Anda
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Izinkan metode-metode tertentu
-  allowedHeaders: ['Content-Type', 'Authorization'], // Header yang diizinkan
+  origin: '*', // Izinkan semua domain (sementara, bisa diganti dengan URL frontend)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-// Middleware
-app.use(cors(corsOptions)); // Apply CORS globally with custom options
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' })); // Untuk mem-parsing JSON dengan limit ukuran
 app.use(express.urlencoded({ limit: '10mb', extended: true })); // Untuk mem-parsing data URL-encoded dengan limit ukuran
 app.use(bodyParser.json()); // Untuk mem-parsing JSON
