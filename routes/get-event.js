453005e15c52afd07bbbd4f83c8bb01861174ff1
ALVIANRML <alvianrmli@gmail.com>
2025-01-28 18:46:44 +0700

    finish

--- routes/get-event.js
+++ routes/get-event.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const connectToDatabase = require("../dbConfig");
 const sql = require("mssql");
 
+// Ambil data event bulan tertentu
 router.get('/get-event', async (req, res) => {
-    try {
-        
-        const pool = await connectToDatabase();
-      const result = await pool.request().query('SELECT * FROM Event');
-      res.status(200).json(result.recordset);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
-  module.exports = router;
+  const { month } = req.query; // Ambil bulan dari query parameter
+  try {
+    const pool = await connectToDatabase();
+    const result = await pool.request()
+      .input('month', sql.Int, month) // Parameter bulan
+      .query('SELECT * FROM Event WHERE MONTH(eventDate) = @month'); // Filter berdasarkan bulan
+    res.status(200).json(result.recordset);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
