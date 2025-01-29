08050fec6194ee16d5949d5709e07584c46f7db8
ALVIANRML <alvianrmli@gmail.com>
2025-01-29 13:57:09 +0700

    finish

--- dbConfig.js
+++ dbConfig.js
@@ -17,7 +17,7 @@ const config = {
     server: process.env.DB_SERVER,
     database: process.env.DB_NAME,
     options: {
-        encrypt: false,
+        encrypt: true,
         enableArithAbort: true,
     },
 };
