b0fc0e77943caca60fdc242714cf2a85ff625296
ALVIANRML <alvianrmli@gmail.com>
2025-01-29 13:19:49 +0700

    finish

--- routes/get-img.js
+++ routes/get-img.js
@@ -27,8 +27,7 @@ router.get('/get-all-images', async (req, res) => {
         }
     } catch (error) {
         console.error('Error fetching images:', error);
-        res.status(500).json({ message: error });
-        // res.status(500).json({  });
+        res.status(500).json({ message: 'Server error' });
     }
 });
 
