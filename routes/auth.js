90c39df75f85f8643f5f694688854f1ef9c24fc8
ALVIANRML <alvianrmli@gmail.com>
2025-01-30 16:48:02 +0700

    finish

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const connectToDatabase = require("../dbConfig");
 const jwt = require("jsonwebtoken");
-
+app.use(cors()); 
 router.post("/login", async (req, res) => {
     const { username, password } = req.body;
 
