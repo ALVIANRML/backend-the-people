296b0079ce5fb1d7267da828aa0a7ec26560a28e
ALVIANRML <alvianrmli@gmail.com>
2025-02-18 07:45:57 +0700

    delete event, fix get/all event

--- routes/event.js
+++ routes/event.js
@@ -1,31 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const connectToDatabase = require("../dbConfig");
+const { req } = require("agent-base");
+const { message } = require("statuses");
+const { v4: uuidv4 } = require('uuid');
 
-// Ambil data event bulan tertentu
+//get event
 router.get('/get-event', async (req, res) => {
   const { month } = req.query;
-
-  // Validasi input
-  if (!month || isNaN(month) || month < 1 || month > 12) {
-    return res.status(400).json({
-      message: "Month parameter is required and must be a number between 1 and 12",
-    });
-  }
+  const client = await connectToDatabase();
 
   try {
-    const client = await connectToDatabase();
-    // Query untuk mendapatkan event berdasarkan bulan
-    const result = await client.query(
-      'SELECT * FROM event WHERE EXTRACT(MONTH FROM eventDate) = $1',
-      [month]
-    );
+    let result;
 
-    // Mengembalikan data event yang ditemukan
+    if (!month || isNaN(month) || month < 1 || month > 12) {
+      // Jika month tidak valid, ambil semua event
+      result = await client.query('SELECT * FROM event');
+    } else {
+      // Jika month valid, ambil event berdasarkan bulan
+      result = await client.query(
+        'SELECT * FROM event WHERE EXTRACT(MONTH FROM eventDate) = $1',
+        [month]
+      );
+    }
+
     if (result.rows.length > 0) {
       return res.status(200).json(result.rows);
     } else {
-     
+      return res.status(404).json({ message: "Event Not Found" });
     }
   } catch (error) {
     console.error("Error fetching events:", error.message);
@@ -33,6 +35,9 @@ router.get('/get-event', async (req, res) => {
   }
 });
 
+
+
+// add event
 router.post('/save-event', async (req, res) => {
   const { eventName, eventDate, eventType, eventLink } = req.body;
 
@@ -76,4 +81,23 @@ router.post('/save-event', async (req, res) => {
     return res.status(500).json({ error: 'Failed to save event' });
   }
 });
+
+// delete event
+router.delete('/delete-event/:id', async (req,res) =>{
+  const {id} = req.params;
+  const client = await connectToDatabase();
+  try{
+  const result = await client.query('DELETE FROM event WHERE id = $1 RETURNING * ', [id]);
+
+  if (result.rowCount == 0){
+    return req.status(404).json ({
+      success:false, message:"Event Not Found"
+    });
+  } else{
+    res.json({success:true, message: "Event Delete Successfully"});
+  }
+  }catch(error){
+    console.error("this is the error" , error)
+  }
+})
 module.exports = router;
